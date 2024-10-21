89645c899326c67c92b84b40126c8d3a052be392
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-10-21 13:54:46 +0200

    class DataConext

---
+++ Cars.API/Cars.Infrastructure/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cars.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cars.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options){ }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
--- Cars.API/Cars.Infrastructure/obj/Debug/net8.0/Cars.Infrastructure.AssemblyInfo.cs
+++ Cars.API/Cars.Infrastructure/obj/Debug/net8.0/Cars.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cars.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d65c9f9316dc8a1cf70844957e8ee15298175e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c949ab73331682860a0b8851baa0a4266799df87")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cars.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cars.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
