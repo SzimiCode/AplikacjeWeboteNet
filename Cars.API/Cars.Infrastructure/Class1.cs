02a893251a965bf701d5aeeeff0f2b1529e6bdab
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-10-21 13:51:13 +0200

    Car Class

--- Cars.API/Cars.Infrastructure/Class1.cs
+++ Cars.API/Cars.Infrastructure/Class1.cs
@@ -2,6 +2,21 @@
 {
     public class Class1
     {
+        public enum FuelType { Petrol, Hybrid, Diesel, LPG }
+        public enum BodyType { Hatchback, Sedan, Kombi, SUV, Roadster }
+        public class Car
+        {
+            public Guid Id { get; set; }
+            public string Brand { get; set; }
+            public string Model { get; set; }
+            public int DoorsNumber { get; set; }
+            public int LuggageCapactiy { get; set; }
+            public int EngineCapactiy { get; set; }
+            public FuelType FuelType { get; set; }
+            public DateTime ProductionDate { get; set; }
+            public double CarFuelConsumption { get; set; }
+            public BodyType BodyType { get; set; }
 
+        }
     }
 }
