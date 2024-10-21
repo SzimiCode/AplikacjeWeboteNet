2e807e479349822c54adffc1557ac262843d7fe7
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-10-21 12:42:26 +0200

    class DataContext

--- Cars2.API/Cars.Infrastructure/obj/Debug/net8.0/Cars2.Infrastructure.AssemblyInfo.cs
+++ Cars2.API/Cars.Infrastructure/obj/Debug/net8.0/Cars2.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cars2.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91d9af78ca064a227a86ca44ee8ec33985f4bf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35762968e7a6837b21300db666c99bb4c2a6dc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cars2.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cars2.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
