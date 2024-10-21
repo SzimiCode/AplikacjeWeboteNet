e0eea02d0fec8e40f31aec69744c6b4f27c6ef42
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-10-21 11:07:42 +0200

    Class libraries

--- Cars.API/Cars.API/obj/Debug/net8.0/Cars.API.AssemblyInfo.fs
+++ Cars.API/Cars.API/obj/Debug/net8.0/Cars.API.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Cars.API")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e04dc0e18816bacda35adba0b227d0a7468bae7")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726dc81fcc734e38876ac189d063f69b75898fde")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Cars.API")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Cars.API")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
